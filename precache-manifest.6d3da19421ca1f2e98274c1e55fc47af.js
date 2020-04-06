2b239ae9b574af05917686c5294c36aa2865f21b
Faraz Shuja <farazshuja@gmail.com>
2020-04-07 00:02:40 +0500

    UI for Home and NewMatch

---
+++ precache-manifest.6d3da19421ca1f2e98274c1e55fc47af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4971559c9146b0c11f1b",
+    "url": "/css/app.310b19d0.css"
+  },
+  {
+    "revision": "46c460c20c584ad9a0c827e2fb2bc2e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2d803f75f7c8a43e046",
+    "url": "/js/about.b9834e49.js"
+  },
+  {
+    "revision": "4971559c9146b0c11f1b",
+    "url": "/js/app.82028d50.js"
+  },
+  {
+    "revision": "c531556efb7aa0840dce",
+    "url": "/js/chunk-07ba15ca.1e81503e.js"
+  },
+  {
+    "revision": "b48e6f446bbaf4234ab4",
+    "url": "/js/chunk-vendors.b2a5c130.js"
+  },
+  {
+    "revision": "44ecd0e980708f1022757f52aa5db3cc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
